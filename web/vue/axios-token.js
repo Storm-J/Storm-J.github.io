b196afd3066c3306fb8632d447dc9d44549bd36a
Storm <1162634877@qq.com>
2020-11-18 15:05:12 +0800

    Delete axios-token.js

--- web/vue/axios-token.js
+++
@@ -1,7 +0,0 @@
-axios.interceptors.request.use(config => {
-  // if ( sessionStorage.getItem("Authorization")) {
-  //   config.headers.common['Authorization'] = sessionStorage.getItem("Authorization")
-  // }
-  config.headers.common['Storm'] = 'JOKER'
-  return config;
-})
